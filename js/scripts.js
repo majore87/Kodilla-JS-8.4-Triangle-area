259523b26f2eafcc7a8c0dae9df2d354e110bda8
Piotr <piotr.krzysztof.wojcik@gmail.com>
2017-02-25 15:23:53 +0100

    JS exercise completed

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var a = prompt('Prompt base of the triangle');
+var h = prompt('Prompt height of the triangle');
+
+var triangleArea = a * h / 2;
+
+alert('Area of a triangle with a base a: ' + a + ' and height h: ' + h + ' is ' + triangleArea);
+console.log('Area of a triangle with a base a: ' + a + ' and height h: ' + h + ' is ' + triangleArea);
